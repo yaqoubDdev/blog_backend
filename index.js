7231d4868294a6bbac36414131a96a6fa49c8f11
yaqoubDdev <yakubswaray@gmail.com>
2025-07-12 18:53:43 +0000

    file format

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-const express = require("express")
-const Blog = require('./model/blogs')
-const app = express()
-const PORT = process.env.PORT
-app.use(express.json())
+const express = require("express");
+const Blog = require("./model/blogs");
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
 
 const logger = (req, res, next) => {
-  console.log("----------------------")
-  console.log("Method:", req.method)
-  console.log("Path:", req.path)
-  console.log("Body:", req.body)
-  console.log("----------------------")
+  console.log("----------------------");
+  console.log("Method:", req.method);
+  console.log("Path:", req.path);
+  console.log("Body:", req.body);
+  console.log("----------------------");
 
-  next()
-}
+  next();
+};
 
 const blogs = [
   {
-    author: "yaqoub", likes: 10,
+    author: "yaqoub",
+    likes: 10,
   },
   {
-    author: "bepo", likes: 50,
+    author: "bepo",
+    likes: 50,
   },
-]
+];
 
-app.use(logger)
+app.use(logger);
 
-app.get('/', (req, res) => {
-  res.send(`<h1>Hello world from app re:</h1>`)
-})
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello world from app re:</h1>`);
+});
 
 app.get("/home/blogs", (req, res) => {
   Blog.find({})
-    .then(blogs => {
-      res.json(blogs)
+    .then((blogs) => {
+      res.json(blogs);
     })
-    .catch(error => res.json(error.message))
-})
+    .catch((error) => res.json(error.message));
+});
 
-app.post('/home/blogs', (req, res) => {
-  const body = req.body
-  if(!body.author || !body.likes){
-    return res.status(400).json({
-      error: "missing author and/or likes"
-    }).end()
+app.post("/home/blogs", (req, res) => {
+  const { author, likes } = req.body;
+  if (!author || !likes) {
+    return res
+      .status(400)
+      .json({
+        error: "missing author and/or likes",
+      })
+      .end();
   }
-  const blog = new Blog({author: body.author , likes: body.likes})
-  blog.save()
-    .then(savedBlog => {
-      res.json(savedBlog)
+  const blog = new Blog({ author, likes });
+  blog
+    .save()
+    .then((savedBlog) => {
+      res.json(savedBlog);
     })
-    .catch(error => {
-      res.json(error.message)
-    })
-})
+    .catch((error) => {
+      res.json(error.message);
+    });
+});
 
-app.listen(
-  PORT, 
-  () => console.log(`App is alive at http://localhost:${PORT}`)
-)
+app.listen(PORT, () => console.log(`App is alive at http://localhost:${PORT}`));
